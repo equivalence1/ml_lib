2fc0084ca13010fba6f27fd83d09ab5c840ed988
noxoomo <vasilij.ershov@gmail.com>
2019-01-22 10:52:12 +0300

    more cover for bds ut

--- cpp/data/ut/data_ut.cpp
+++ cpp/data/ut/data_ut.cpp
@@ -49,26 +49,28 @@ TEST(FeaturesTxt, TesGrid) {
 
 //run it from root
 TEST(FeaturesTxt, TesBinarize) {
-    auto ds = loadFeaturesTxt("test_data/featuresTxt/train");
-    EXPECT_EQ(ds.samplesCount(), 12465);
-    EXPECT_EQ(ds.featuresCount(), 49);
+    for (int32_t groupSize : {2, 4, 8, 11, 16, 32}) {
+        auto ds = loadFeaturesTxt("test_data/featuresTxt/train");
+        EXPECT_EQ(ds.samplesCount(), 12465);
+        EXPECT_EQ(ds.featuresCount(), 49);
 
-    {
-        BinarizationConfig config;
-        config.bordersCount_ = 32;
-        auto grid = buildGrid(ds, config);
+        {
+            BinarizationConfig config;
+            config.bordersCount_ = 32;
+            auto grid = buildGrid(ds, config);
 
-        for (int32_t i = 0; i < grid->nzFeaturesCount(); ++i) {
-            EXPECT_LE(grid->binCount(i), 33);
-        }
+            for (int32_t i = 0; i < grid->nzFeaturesCount(); ++i) {
+                EXPECT_LE(grid->binCount(i), 33);
+            }
 
-        auto bds = binarize(ds, grid, 8);
+            auto bds = binarize(ds, grid, groupSize);
 
-        for (int64_t f = 0; f < grid->nzFeaturesCount(); ++f) {
-            int64_t origFeatureIndex = grid->origFeatureIndex(f);
-            bds->visitFeature(f, [&](int64_t lineIdx, int64_t bin) {
-                EXPECT_EQ(computeBin(ds.sample(lineIdx).get(origFeatureIndex), grid->borders(f)), bin);
-            });
+            for (int64_t f = 0; f < grid->nzFeaturesCount(); ++f) {
+                int64_t origFeatureIndex = grid->origFeatureIndex(f);
+                bds->visitFeature(f, [&](int64_t lineIdx, int64_t bin) {
+                    EXPECT_EQ(computeBin(ds.sample(lineIdx).get(origFeatureIndex), grid->borders(f)), bin);
+                });
+            }
         }
     }
 }
--- cpp/models/ut/models_ut.cpp
+++ cpp/models/ut/models_ut.cpp
@@ -12,41 +12,43 @@
 
 //run it from root
 TEST(FeaturesTxt, ApplyFloatAndBinarizedOtTest) {
-    auto ds = loadFeaturesTxt("test_data/featuresTxt/train");
-    EXPECT_EQ(ds.samplesCount(), 12465);
-    EXPECT_EQ(ds.featuresCount(), 49);
+    for (int32_t groupSize : {2, 4, 8, 16, 17, 32}) {
+        auto ds = loadFeaturesTxt("test_data/featuresTxt/train");
+        EXPECT_EQ(ds.samplesCount(), 12465);
+        EXPECT_EQ(ds.featuresCount(), 49);
 
+        BinarizationConfig config;
+        config.bordersCount_ = 32;
+        auto grid = buildGrid(ds, config);
 
-    BinarizationConfig config;
-    config.bordersCount_ = 32;
-    auto grid = buildGrid(ds, config);
+        for (int32_t i = 0; i < grid->nzFeaturesCount(); ++i) {
+            EXPECT_LE(grid->binCount(i), 33);
+        }
 
-    for (int32_t i = 0; i < grid->nzFeaturesCount(); ++i) {
-        EXPECT_LE(grid->binCount(i), 33);
+        auto bds = cachedBinarize(ds, grid, groupSize);
+
+        for (int32_t firstF = 0; firstF < grid->nzFeaturesCount(); firstF += 6) {
+            std::vector<BinaryFeature> features;
+            for (int32_t i = firstF; i < std::min<int32_t>(firstF + 6, grid->nzFeaturesCount()); ++i) {
+                features.emplace_back(i, grid->binCount(i) / 2);
+            }
+            Vec values(64);
+            for (int64_t i = 0; i < 64; ++i) {
+                values.arrayRef()[i] = i;
+            }
+
+            ObliviousTree tree(grid, features, values);
+
+            Vec toFromBds(ds.samplesCount());
+            Vec toFromDs(ds.samplesCount());
+
+            tree.apply(bds, Mx(toFromBds, ds.samplesCount(), 1));
+            tree.apply(ds, Mx(toFromDs, ds.samplesCount(), 1));
+            EXPECT_EQ(toFromBds, toFromDs);
+        }
+
     }
 
-    auto bds = cachedBinarize(ds, grid, 8);
-
-    std::vector<BinaryFeature> features;
-    for (int32_t i = 0; i < 6; ++i) {
-        features.emplace_back(i, grid->binCount(i) / 2);
-    }
-    Vec values(64);
-    for (int64_t i = 0; i < 64; ++i) {
-        values.arrayRef()[i] = i;
-    }
-
-    ObliviousTree tree(grid, features, values);
-
-
-    Vec toFromBds(ds.samplesCount());
-    Vec toFromDs(ds.samplesCount());
-
-    tree.apply(bds, Mx(toFromBds, ds.samplesCount(), 1));
-    tree.apply(ds, Mx(toFromDs, ds.samplesCount(), 1));
-
-
-    EXPECT_EQ(toFromBds, toFromDs);
 }
 
 
