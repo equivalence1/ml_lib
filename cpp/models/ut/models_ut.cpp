214d0291faef80efdf1d9a2f01c358318c8fe8dd
Dmitriy <selp@dmitriy.msi-gs65>
2019-02-09 13:59:49 +0300

    model test

--- cpp/models/ut/models_ut.cpp
+++ cpp/models/ut/models_ut.cpp
@@ -7,6 +7,7 @@
 #include <data/grid_builder.h>
 #include <data/binarized_dataset.h>
 #include <models/oblivious_tree.h>
+#include <vec_tools/transform.h>
 
 #define EPS 1e-5
 
@@ -51,49 +52,45 @@ TEST(FeaturesTxt, ApplyFloatAndBinarizedOtTest) {
 
 }
 
-TEST(FeaturesTxt, ApplyFloatAndBinarizedOtTest) {
-    for (int32_t groupSize : {2, 4, 8, 16, 17, 32}) {
-        auto ds = loadFeaturesTxt("test_data/featuresTxt/train");
-        EXPECT_EQ(ds.samplesCount(), 12465);
-        EXPECT_EQ(ds.featuresCount(), 49);
+TEST(FeaturesTxt, Gradient) {
+    auto ds = loadFeaturesTxt("test_data/featuresTxt/train");
+    EXPECT_EQ(ds.samplesCount(), 12465);
+    EXPECT_EQ(ds.featuresCount(), 49);
 
-        BinarizationConfig config;
-        config.bordersCount_ = 32;
-        auto grid = buildGrid(ds, config);
+    BinarizationConfig config;
+    config.bordersCount_ = 32;
+    auto grid = buildGrid(ds, config);
 
-        for (int32_t firstF = 0; firstF < grid->nzFeaturesCount(); firstF += 6) {
-            std::vector<BinaryFeature> features;
-            for (int32_t i = firstF; i < std::min<int32_t>(firstF + 6, grid->nzFeaturesCount()); ++i) {
-                features.emplace_back(i, grid->conditionsCount(i) / 2);
-            }
-            Vec values(64);
-            for (int64_t i = 0; i < 64; ++i) {
-                values.arrayRef()[i] = i;
-            }
-
-            Vec weg(1 << features.size());
-            for (int i = 0; i < weg.dim(); ++i) {
-                weg.set(i, 2.0 * std::rand() / RAND_MAX - 1.0 );
-            }
-            ObliviousTree tree(grid, features, values, weg);
-            
-            double delta = 0.001;
-            for (int64_t k = 0; k < ds.samplesCount(); ++k) {
-                Vec grad = Vec(ds.sample(0).dim());
-                tree.grad(ds.sample(k), grad);
-                Vec approx = Vec(grad.dim());
-                for (int64_t p = 0; p < grad.dim(); ++p) {
-                    Vec tmp = Vec(grad.dim());
-                    VecTools::copyTo(ds.sample(k), tmp);
-                    tmp.set(p, tmp.get(p) + delta);
-                    approx.set(p, (tree.value(tmp) - tree.value(ds.sample(k))) / delta);
-                    assert(std::abs(approx.get(p) - grad.get(p)) <= 0.001);
-                }
-            }
+    for (int32_t firstF = 0; firstF < grid->nzFeaturesCount(); firstF += 6) {
+        std::vector<BinaryFeature> features;
+        for (int32_t i = firstF; i < std::min<int32_t>(firstF + 6, grid->nzFeaturesCount()); ++i) {
+            features.emplace_back(i, grid->conditionsCount(i) / 2);
+        }
+        Vec values(64);
+        for (int64_t i = 0; i < 64; ++i) {
+            values.arrayRef()[i] = i;
         }
 
-    }
+        Vec weg(1 << features.size());
+        for (int i = 0; i < weg.dim(); ++i) {
+            weg.set(i, 2.0 * std::rand() / RAND_MAX - 1.0 );
+        }
+        ObliviousTree tree(grid, features, values, weg);
 
+        double delta = 0.001;
+        for (int64_t k = 0; k < ds.samplesCount(); ++k) {
+            Vec grad = Vec(ds.sample(0).dim());
+            tree.grad(ds.sample(k), grad);
+            Vec approx = Vec(grad.dim());
+            for (int64_t p = 0; p < grad.dim(); ++p) {
+                Vec tmp = Vec(grad.dim());
+                VecTools::copyTo(ds.sample(k), tmp);
+                tmp.set(p, tmp.get(p) + delta);
+                approx.set(p, (tree.value(tmp) - tree.value(ds.sample(k))) / delta);
+                assert(std::abs(approx.get(p) - grad.get(p)) <= 0.001);
+            }
+        }
+    }
 }
 
 
