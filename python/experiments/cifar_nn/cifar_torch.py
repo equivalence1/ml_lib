30c9d7bf32957b19dee19b9c857e6921a1b1696f
Dmitrii Kravchenko <equivalence1@gmail.com>
2019-03-16 01:14:01 +0300

    fix path

--- python/experiments/cifar_nn/cifar_torch.py
+++ python/experiments/cifar_nn/cifar_torch.py
@@ -41,12 +41,12 @@ def get_data_loaders():
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = torchvision.datasets.CIFAR10(root='../resources', train=True,
+    trainset = torchvision.datasets.CIFAR10(root='../../resources/cifar10', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                               shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root='../resources', train=False,
+    testset = torchvision.datasets.CIFAR10(root='../../resources/cifar10', train=False,
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                              shuffle=False, num_workers=2)
