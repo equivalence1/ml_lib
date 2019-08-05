6d596780b93271b59741f297b9707cecc2659101
equivalence1 <equivalence1@gmail.com>
2019-08-05 22:29:38 +0300

    cuda compilation params

--- cpp/models/polynom/polynom_cuda/compile.sh
+++ cpp/models/polynom/polynom_cuda/compile.sh
@@ -1,2 +1,2 @@
-/usr/local/cuda-10.0/bin/nvcc  -c -o libsoft_polynom.so  --ptxas-options=-v --compiler-options '-fPIC' -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70   -gencode arch=compute_35,code=compute_35  -gencode arch=compute_61,code=compute_61 -gencode arch=compute_70,code=compute_70  --shared soft_polynom.cu
+/usr/local/cuda-10.1/bin/nvcc  -c -o libsoft_polynom.so  --ptxas-options=-v --compiler-options '-fPIC' -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75  -gencode arch=compute_35,code=compute_35  -gencode arch=compute_61,code=compute_61  --shared soft_polynom.cu
 
